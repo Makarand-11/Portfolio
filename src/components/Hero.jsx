ed7035f7cdfec4c29c709ec299a702d7c26e7c70
Makarand Nagvekar <makarandnagvekar1036@gmail.com>
2024-09-01 19:57:12 +0530

    UI changes 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ import { motion } from "framer-motion"
 
 const Hero = () =>{
     return(
-        <section className="mt-8 flex min-h-screen flex-wrap items-center">
+        <section className="flex min-h-screen flex-wrap items-center mt-14">
             <motion.div 
                 initial={{ opacity:0, x:-50 }}
                 animate={{ opacity:1, x:0 }}
                 transition={{ duration:1 }}
-                className="mt-8 w-full md:w-1/2">
+                className="w-full md:w-1/2">
                 <h2 className="my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]">
                     {HERO.name}
                 </h2>

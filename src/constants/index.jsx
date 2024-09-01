ef511a8dc3e7672e8facfe4d068e38f0b104c91e
Makarand Nagvekar <makarandnagvekar1036@gmail.com>
2024-09-01 15:51:58 +0530

    Added Working Link to My first Project

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -44,7 +44,7 @@ export const PROJECTS = [
     description:
       "A personal portfolio website built with React and Tailwind CSS to showcase my skills, projects, and contact information.",
     image: projectImage1,
-    githubLink: "https://github.com/user/personal-portfolio",
+    githubLink: "https://github.com/Makarand-11/Portfolio",
   },
   {
     id: 2,
